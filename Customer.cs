28c45b54efee0f499a68ae56f0459ee9e2b90f0a
SashaGolovchenko <135842969+SashaGolovchenko@users.noreply.github.com>
2024-01-08 17:19:49 +0300

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    public class Customer
+    {
+        public DateTime Date { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int PeopleCount { get; set; }
+        public int RoomNumber { get; set; }
+        public string PassportData { get; set; }
+        public decimal RoomPrice { get; set; }
+        public string CustomerCode { get; set; }
+        public int RoomLevel { get; set; }
+    }
+}
